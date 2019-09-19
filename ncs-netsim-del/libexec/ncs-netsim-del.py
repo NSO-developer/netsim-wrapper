858a5fdece12f130f93f7cd6b62afbf02d22b893
kirankotari <kirankotari@live.com>
2019-09-19 16:45:51 +0530

    added TODOs

--- ncs-netsim-del/libexec/ncs-netsim-del.py
+++ ncs-netsim-del/libexec/ncs-netsim-del.py
@@ -4,3 +4,13 @@ def main(*argv):
     print(argv)
 
 main(sys.argv[1:])
+
+# TODO: need to check the flags like -v, --version, -h, --help as a first arg 
+# TODO: if the arg list contains --dir need to read the "NetsimDir" path
+# TODO: need to store the devices names need to be deleted. 
+
+
+# TODO: need to deleting the device folders
+# TODO: need to update the .netsiminfo file
+
+# TODO: need to add logging messages
