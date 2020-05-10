e60f1de81a20b1fd7980baaaa198b4a9b2d64f35
kirankotari <kirankotari@live.com>
2020-05-10 07:06:17 +0530

    updated setup.py similar to pingping

--- setup.py
+++ setup.py
@@ -3,37 +3,46 @@ from os import path
 from io import open
 
 here = path.abspath(path.dirname(__file__))
+reqs = []
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-	long_description = f.read()
+    long_description = f.read()
 
 with open(path.join(here, '.version'), encoding='utf-8') as f:
-	version = f.read()
+    version = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    read_lines = f.readlines()
+    reqs = [each.strip() for each in read_lines]
 
 setup(
-	name = 'ncs-netsim2',
-	version = version,
-	description = "Introducing a new command to delete the ncs-netsim devices.  It was a hardened when the feature was missing, We were forced to delete the complete simulated network and recreate again with the rest of devices.",
+    name = 'ncs-netsim2',
+    version = version,
+    description = "ncs-netsim2 is a powerful simulator tool in python. It's a wrapper of ncs-netsim tool with added features",
     long_description = long_description,
-	long_description_content_type = 'text/markdown',
-	url = 'https://github.com/kirankotari/ncs-netsim2.git',
-	author = 'Kiran Kumar Kotari',
-	author_email = 'kirankotari@live.com',
-	classifiers = [
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Topic :: Software Development :: Build Tools',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		],
-	keywords = 'Netsim delete device',
-	packages = find_packages(exclude=['tests']),
-	package_dir = {'ncs-netsim2': 'ncs-netsim2'},
-	package_data = {'ncs-netsim2': []},
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/kirankotari/ncs-netsim2.git',
+    author = 'Kiran Kumar Kotari',
+    author_email = 'kirankotari@live.com',
+    entry_points={
+        'console_scripts': [
+            'ncs-netsim2 = ncs-netsim2.netsim2:run' # TODO: need to updated the command
+        ],
+    },
+    install_requires=reqs,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    keywords = 'Ncs-Netsim, Ncs-Netsim2, delete netsim device',
+    packages = find_packages(where='.', exclude=['tests']),
+    include_package_data=True,
 )
 
